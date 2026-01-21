96b9f1825be69680411daa15430403b67e744c2a
Vicky <40175721+vbatcnam@users.noreply.github.com>
2026-01-21 21:46:31 +0100

    Refactor pauseReguliere.py for improved functionality

--- pauseReguliere.py
+++ pauseReguliere.py
@@ -1,35 +1,71 @@
 from tkinter import *
 from tkinter.font import Font
+from PIL import Image, ImageTk
 
+# programmer les temps
+#---------------------
 nbreSecondePeriodeTravail = 15*60
 nbreSecondePause = 60
 nbreSecondeDelaiAvertissement = 20
-nbreSecondeDelaiAvertissementSansOK = 5*60
-# ~ nbreSecondePeriodeTravail = 7
+nbreSecondeDelaiAvertissement_default = 5*60
+
+#====== pour test ====================
+# ~ nbreSecondePeriodeTravail = 7 # affiche 0 secondes
 # ~ nbreSecondePause = 5
 # ~ nbreSecondeDelaiAvertissement = 4
-# ~ nbreSecondeDelaiAvertissementSansOK = 10
+# ~ nbreSecondeDelaiAvertissement_default = 10
+#--------------------------------------------------
 
+# textes
+#--------
+text_lancementPause = 'Démmarer la pause dans %s secondes' % nbreSecondeDelaiAvertissement
+text_lancementpauseAutomatique = 'Pause automatique dans %s secondes' % nbreSecondeDelaiAvertissement_default
+
+#graphisme
+#--------------
+couleur_text = 'black'
+couleur_textBouton = 'white'
+couleur_boutons = '#3283D3'
+fond_fenetre_pause ='#ffff89'
+fond_fenetre_pauseFini ='#92ff92'
+fond_fenetre_alerte ='#ffccaa'
+
+#images
+img_sport = 'sport.jpg'
+img_travail = 'travail.png'
+
+
+# Fenêtre de mise en pause
+#-------------------------
 def petiteFenetre():
 	root.geometry('400x300+20+20')
-	lab['wraplength'] = 300
+	texte['wraplength'] = 300
 
+# Fenêtre de pause
+#-------------------------
 def grandeFenetre():
 	root.geometry('1560x830+20+20')
-	lab['wraplength'] = 0
+	texte['wraplength'] = 0
 
+# Basculer vers l'avertissement personnalisé
+#-------------------------------------------
 def switchToAvertissement():
 	root.state('normal')
+	root['bg']=fond_fenetre_alerte
 	petiteFenetre()
-	lab['text'] = 'Pause dans %s secondes !' % nbreSecondeDelaiAvertissement
+	texte['text'] = text_lancementpauseAutomatique
 	bouton['state'] = 'normal'
-	bouton['bg'] = '#C0C0FF'
+	bouton['text']=text_lancementPause
+	bouton['bg'] = couleur_boutons
 	bouton['command'] = switchToPrePause
 	global delaiAnnulable
-	delaiAnnulable = root.after(1000 * nbreSecondeDelaiAvertissementSansOK, switchToPause)
+	delaiAnnulable = root.after(1000 * nbreSecondeDelaiAvertissement_default, switchToPause)
 
+# lancer la pause
+#-------------------
 def switchToPrePause():
 	global delaiAnnulable
+	root['bg']=fond_fenetre_pause
 	root.after_cancel(delaiAnnulable)
 	root.state('withdrawn')
 	root.after(1000 * nbreSecondeDelaiAvertissement, switchToPause)
@@ -37,37 +73,56 @@ def switchToPrePause():
 def switchToPause():
 	root.state('normal')
 	grandeFenetre()
-	lab['text'] = 'Maintenant : pause de %s secondes !' % nbreSecondePause
+	texte['text'] = 'Maintenant : pause de %s secondes !' % nbreSecondePause
+	# ~ img = Image.open(img_sport) 
+	# ~ img_tk = ImageTk.PhotoImage(img)
+	# ~ label = Label(root, image=img_tk) 
+	# ~ label.pack()
 	bouton.pack_forget()
 	root.after(1000 * nbreSecondePause, switchToPreRepriseTravail)
 
 def switchToPreRepriseTravail():
-	lab['text'] = 'C\'est parti pour une période de %s minutes !' % (nbreSecondePeriodeTravail // 60)
+	root['bg']=fond_fenetre_pauseFini
+	texte['text'] = 'C\'est parti pour une période de %s minutes !' % (nbreSecondePeriodeTravail // 60)
 	bouton.pack()
 	bouton['state'] = 'normal'
-	bouton['bg'] = '#C0FFC0'
+	bouton['text']='OK'
+	bouton['bg'] = couleur_boutons
 	bouton['command'] = switchToTravail
 
 def switchToTravail():
 	root.state('withdrawn')
 	root.after(1000 * nbreSecondePeriodeTravail, switchToAvertissement)
 
-root = Tk()
+# main
+#==========
+# Géré par TK
+#--------------
+
+# fabrication de la fenêtre
+root = Tk() 
 root.overrideredirect(1)
-root.attributes("-topmost", True)
+root.attributes("-topmost", True) # mise en avant plan de tous les programmes qui tournent
 
-lab = Label()
-lab['font'] = ("Arial", 16, "bold")
-lab['fg'] = '#804000'
-lab['padx'] = 100
-lab['pady'] = 100
-lab.pack()
 
-bouton = Button(text='OK')
+# fabrication d'un texte
+texte = Label()
+texte['font'] = ("Arial", 16, "bold")
+texte['fg'] = couleur_text
+# ~ texte['padx'] = 20 #padding
+# ~ texte['pady'] = 20 #padding
+# ~ texte['ipadx']= 50
+# ~ texte['ipady']= 50
+
+texte.pack(padx=40, pady=40, ipadx=20, ipady=20) # pour afficher sur la fenêtre
+
+# fabrique un bouton
+bouton = Button(text='')
 bouton['padx'] = 20
 bouton['pady'] = 20
-bouton.pack()
+bouton.pack()  # pour afficher sur la fenêtre
 
+# lance le programme de pause
 switchToTravail()
 
 # root.mainloop()
